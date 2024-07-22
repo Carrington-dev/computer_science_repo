21d0f7067e59c0878dfaae17d138f5e652cc0ec6
Carrington-dev <crn96m@gmail.com>
2024-07-22 19:33:02 +0200

    AWS Data Structures and Algorithms Fixed

--- Problem-Solving/Trees/count_customers.py
+++ Problem-Solving/Trees/count_customers.py
@@ -1,8 +1,7 @@
-def re_c():
+def re_c(file_name):
     counter = 0
-    with open("reads.txt", "r") as f:
+    with open(file_name, "r") as f:
         context = dict()
-
         for i in f.readlines():
             id = (i[0:-1].split(", ")[1])
             if context.get(id) != None:
@@ -12,4 +11,4 @@ def re_c():
         
     return counter
 
-print(re_c())
+print(re_c("reads.txt"))
