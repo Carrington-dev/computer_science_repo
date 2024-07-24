e4d35cd4fe945d2412a8d554f88823c6421f656d
Carrington-dev <crn96m@gmail.com>
2024-07-24 12:10:54 +0200

    AWS System-Design, Low Level Design and logical problem solving

--- Problem-Solving/Arrays/mergeLists.py
+++ Problem-Solving/Arrays/mergeLists.py
@@ -17,35 +17,35 @@ class Solution:
         # print("nums1 = ", nums1, "nums2 = ", nums2)
         i = 0
         j = 0
-        new_list = [ 0 for i in range(len(nums1)+len(nums2))]
+        new_list = []
         while i < len(nums1) and j < len(nums2):
             if nums1[i] < nums2[j]:
-                new_list[ i + j ] = nums1[i]
+                new_list.append(nums1[i])
                 i += 1
             elif nums1[i] > nums2[j]:
-                new_list[ i + j ] = nums2[j]
+                new_list.append(nums2[j])
                 j += 1
-            else:
-                new_list[ i + j ] = nums1[i]
-                new_list[ i + j + 1 ] = nums2[j]
+            elif nums1[i] == nums2[j]:
+                new_list.append(nums1[i])
+                # new_list[ i + j + 1 ] = nums2[j]
                 i += 1
                 j += 1
 
         
         while i < len(nums1):
-            new_list[ i + j ] = nums1[i]
+            new_list.append(nums1[i])
             i += 1
 
 
         while j < len(nums2):
-            new_list[ i + j ] = nums2[j]
+            new_list.append(nums2[j])
             j += 1
 
         return new_list
     
 
-print(Solution().findMergeSortedArrays([1,2], [3,4]))
+print(Solution().findMergeSortedArrays([1,2], [2, 3,4]))
 print(Solution().findMergeSortedArrays([i for i in range(0, 13) if i % 2 == 1 ], [i for i in range(0, 13) if i % 2 == 0]))
 
-print(Solution().findMedianSortedArrays([1,2], [3,4]))
-print(Solution().findMedianSortedArrays([i for i in range(0, 13) if i % 2 == 1 ], [i for i in range(0, 13) if i % 2 == 0]))
+# print(Solution().findMedianSortedArrays([1,2], [3,4]))
+# print(Solution().findMedianSortedArrays([i for i in range(0, 13) if i % 2 == 1 ], [i for i in range(0, 13) if i % 2 == 0]))
