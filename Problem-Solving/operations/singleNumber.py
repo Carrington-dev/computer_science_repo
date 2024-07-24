7358b433d3d771164673242bea85777d6b94af7f
Carrington-dev <crn96m@gmail.com>
2024-07-24 12:42:55 +0200

    SingleNumber

---
+++ Problem-Solving/operations/singleNumber.py
@@ -0,0 +1,10 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        counter = { v:k for k, v in counter.items() }
+        return counter[1]
+    
+print(Solution().singleNumber(nums = [4,1,2,1,2]))
