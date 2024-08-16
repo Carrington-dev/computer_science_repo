a67275ac4fb03c91a5a7f02aacd48cb853b83e27
Carrington-dev <crn96m@gmail.com>
2024-08-16 14:34:59 +0200

    Added Paused Reverse Binary (Inunderstandable concept

---
+++ Problem-Solving/recursion/mergeKLists.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """
+        How to merge k Lists
+
+        Strategy.
+        1. I would join two list at a time
+        """
---
+++ Problem-Solving/recursion/reverseBits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return n
+    
+
+print(Solution().reverseBits(00000010100101000001111010011100))
+print(Solution().reverseBits(00000010100101000001111010011100))
+print(Solution().reverseBits(00000010100101000001111010011100))
