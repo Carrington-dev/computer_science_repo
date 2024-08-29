75add07bedc351314810c3624522f0e72d3ce198
Carrington-dev <crn96m@gmail.com>
2024-08-29 08:49:35 +0200

    Added Fib Dynamic Programming

--- Problem-Solving/DynamicProgramming/fibonacci.py
+++ Problem-Solving/DynamicProgramming/fibonacci.py
@@ -1,9 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
         arr =  [0, 1]
-        for i in range(2, n):
+        for i in range(2, n + 1):
             arr.append(arr[i - 1] + arr[i - 2])
-        print(arr)
-        return arr[n-1]
+        return arr[n]
 
-   
+print(Solution().fib(4))
