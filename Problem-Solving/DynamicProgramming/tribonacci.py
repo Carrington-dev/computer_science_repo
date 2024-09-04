e931eb5c612e34dbc1e2fdee8e871fcbe44f3561
Carrington-dev <crn96m@gmail.com>
2024-09-04 22:22:04 +0200

    Added dynamic tribonacci

--- Problem-Solving/DynamicProgramming/tribonacci.py
+++ Problem-Solving/DynamicProgramming/tribonacci.py
@@ -1,17 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         tribonacci_items = [0, 1, 1]
-        for i in range(2, n+1):
+        for i in range(3, n + 1):
             tribonacci_items.append(tribonacci_items[i - 1] + tribonacci_items[i - 2] + tribonacci_items[i - 3])
         
-        return tribonacci_items[n + 1 ]
+        return tribonacci_items[n]
         
 
 # T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
 print(Solution().tribonacci(0))
 print(Solution().tribonacci(25))
-print(Solution().tribonacci(2))
-print(Solution().tribonacci(3))
-print(Solution().tribonacci(4))
-print(Solution().tribonacci(5))
+# print(Solution().tribonacci(2))
+# print(Solution().tribonacci(3))
+# print(Solution().tribonacci(4))
+# print(Solution().tribonacci(5))
--- Problem-Solving/tribonacci.py
+++ Problem-Solving/tribonacci.py
@@ -5,3 +5,5 @@ class Solution:
         elif n == 2:
             return 1
         return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+    
+print(Solution().tribonacci(25))
