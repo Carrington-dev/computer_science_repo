11498ca03740dd2908deadd3911545fa1f439bfa
Carrington-dev <crn96m@gmail.com>
2025-02-20 23:10:19 +0200

    Added DBMS folder

---
+++ Journey-to-silicon-valley/Week_1/day-1/path-sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    public:
+        bool hasPathSum(TreeNode* root, int targetSum) {
+            
+        }
+
+        int findSum(TreeNode* root, int sum = 0){
+            if(root == nullptr) return sum;
+            
+        }
+};
