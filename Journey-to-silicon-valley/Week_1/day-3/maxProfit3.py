34eefd119234839fda42feb4cb5f44914c211a5e
Carrington-dev <crn96m@gmail.com>
2025-02-24 15:16:03 +0200

    Added maxProfit Day 3 N/C

--- Journey-to-silicon-valley/Week_1/day-3/maxProfit3.py
+++ Journey-to-silicon-valley/Week_1/day-3/maxProfit3.py
@@ -4,4 +4,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        buying_price = prices[0]
+        profit = 0
+
+        for i in range(1, len(prices)):
+            pass
+
+        return profit
