69ca313576b79567b69d7caf21346c3863374ba6
Carrington-dev <crn96m@gmail.com>
2025-02-24 09:57:59 +0200

    Solving maxProfit stage 1

---
+++ maxProfit.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum = prices[0]
+        index = 0
+        profit = 0
+
+        for i in range(len(prices)):
+            if prices[i] < minimum:
+                minimum = prices[i]
+                index = i
+            
+        
+        profit = max(prices[index:]) - minimum
+
+        return profit
+
+
+
+print(Solution().maxProfit( prices = [7,1,5,3,6,4]))
+print(Solution().maxProfit( prices = [7,6,4,3,1]))
+print(Solution().maxProfit( prices = [2,4,1]))
