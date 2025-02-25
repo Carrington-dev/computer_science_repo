36b47ed1ddfafd853aa0ca25158986f0d52c0a2e
Carrington-dev <crn96m@gmail.com>
2025-02-25 08:45:08 +0200

    Initialized merge sorted array by reference in-place

---
+++ Journey-to-silicon-valley/AWS-2/mergeSortedArray.py
@@ -0,0 +1,11 @@
+# 88. Merge Sorted Array
+
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
--- Journey-to-silicon-valley/Week_1/day-3/maxProfit3.py
+++ Journey-to-silicon-valley/Week_1/day-3/maxProfit3.py
@@ -6,8 +6,16 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buying_price = prices[0]
         profit = 0
+        profits = []
 
-        for i in range(1, len(prices)):
-            pass
-
+        for i in range(1, len(prices) - 1):
+            if prices[i + 1] - prices[i] > 0:
+                profits.append(prices[i + 1] - prices[i])
+        
+        profits = sorted(profits)[::-1]
+        profit = sum(profits[:2])
         return profit
+    
+
+print(Solution().maxProfit(prices = [3,3,5,0,0,3,1,4]))
+print(Solution().maxProfit(prices = [1,2,3,4,5]))
