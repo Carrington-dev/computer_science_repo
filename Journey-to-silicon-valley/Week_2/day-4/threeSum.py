7d8cbfc068d21bbf12faf942c17feafcbd8dccc4
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-03-14 09:12:06 +0200

    Added S1 D3 W2 Not Optimum

---
+++ Journey-to-silicon-valley/Week_2/day-4/threeSum.py
@@ -0,0 +1,19 @@
+# 15. 3Sum
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        context = set()
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                for k in range(len(nums)):
+                    if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
+                        context.add((nums[i], nums[j], nums[k]))
+        
+        return list(context)
+
+print(Solution().threeSum(nums = [-1,0,1,2,-1,-4]))
+print(Solution().threeSum(nums = [0,1,1]))
+print(Solution().threeSum(nums = [0,0,0]))
