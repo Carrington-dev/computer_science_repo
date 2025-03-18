73d45508a3e4bfe2260c4b57d68132a1f8a62a1f
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-03-18 11:23:16 +0200

    Update rightSideView.py

--- Journey-to-silicon-valley/Week_2/day-7/rightSideView.py
+++ Journey-to-silicon-valley/Week_2/day-7/rightSideView.py
@@ -1,1 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        all_items = []
+        self.rightSideViewId(root, all_items)
+        return all_items
 
+    def rightSideViewId(self, root: Optional[TreeNode], listItems) -> List[int]:
+        if root == None:
+            return
+        
+        listItems.append(root.val)
+        self.rightSideViewId(root.right, listItems)
+        # self.rightSideViewId(root.left, listItems, False)
